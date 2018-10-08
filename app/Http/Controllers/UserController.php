658379fba2864a9201e8e3f078cf023acabe2ce4
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-07 23:47:11 -0300

    Adicionando show ao UserController - issue1
    Adicionando a função show à classe UserController para apresentação dos
    dados de um dado usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,25 @@ use App\User;
 
 class UserController extends Controller
 {
-    public function index(User $user)
+
+    private $user;
+
+    public function __construct(User $user)
     {
-        $users = $user->all();   
+        $this->user = $user;
+    }
+
+    public function index()
+    {
+        $users = $this->user->all();   
         
         return view('user.index', compact('users'));
+    }
+
+    public function show($id)
+    {
+        $user = $this->user->find($id);
+
+        return view('user.show', compact('user'));
     }
 }
