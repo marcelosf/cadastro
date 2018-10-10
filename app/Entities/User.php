c458196ef2d2d710b794e5efb59562275c26145a
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-09 22:55:26 -0300

    Utilização do Entity User - issue #5
    Alterando configuração e substituindo a classe User padrão do laravel
    pelo Entity User criado pelo pacote repository.

---
+++ app/Entities/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class User.
+ *
+ * @package namespace App\Entities;
+ */
+class User extends Authenticatable implements Transformable
+{
+    use TransformableTrait;
+    use Notifiable;
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+}
