58e2614252997fe625a35c6772467fc6e8dc60d7
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-14 19:34:10 -0300

    Atualização do factory para users-issue #14
    Adicionando novos parâmetros de usuários no factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,5 +19,18 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'picture' => $faker->uuid,
+        'membership_number' => $faker->randomNumber(5),
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->streetName,
+        'city' => $faker->city,
+        'state' => $faker->state,
+        'membership_year' => $faker->year(),
+        'birth_date' => $faker->date('Y-m-d'),
+        'rg' => $faker->randomNumber(7),
+        'cpf' => $faker->randomNumber(7),
+        'bloody_type' => $faker->randomLetter,
+        'health_plan' => $faker->randomElement(['Amil', 'GoldenCross', 'Porto Seguro']),
+        'cbm_where' => $faker->randomElement(['CAP', 'GPM', 'CEU']),
     ];
 });
